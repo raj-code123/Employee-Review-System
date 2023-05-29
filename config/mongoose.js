5d2b22960c467cf60e63355033cf9b600d0d6eaf
raj <rajdudhatra1999@gmail.com>
2023-05-29 15:16:54 +0530

    add mongoose add

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const {MONGODB_URL} = process.env;
-mongoose.connect(MONGODB_URL,{
+mongoose.connect(MONGODB_URL.toString() ,{
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
-console.log(MONGODB_URL);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"))
